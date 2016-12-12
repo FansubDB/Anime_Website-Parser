66dca1e16afc797345a11acb262fc1aad9920119
FansubDB <fansubdb@mail-me.com>
2016-12-12 11:25:49 +0100

    Update livechart_parser.php

--- livechart_parser.php
+++ livechart_parser.php
@@ -5,7 +5,11 @@ include("HtmlDomParser.php");
 $dom = HtmlDomParser::file_get_html('livechart.html');
 
 foreach ($dom->find('h3') as $h) {
-	echo $h->innertext;
+	echo '{';
+	echo '"name": "' . $h->innertext . '",';
+	echo '"image": "",';
+	echo '"group": []';
+	echo '}';
 	echo '<br>';
 }
 ?>
